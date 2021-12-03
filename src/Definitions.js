3392312f5e0b1e997a3a4d216c048452b948243e
kiriri <willian.pacheco@solidaridadnetwork.org>
2021-12-03 15:05:51 -0300

    definiçoes

--- src/Definitions.js
+++ src/Definitions.js
@@ -6,23 +6,25 @@ const definitions = (props) => {
             <div class="card-body" style={{fontSize:"1rem"}}>
                 <h5 className="card-title mt-2">Definições</h5>
                 <dl>
-                <dt > (SD): Sintagma Determinante</dt>
+                <dt > (IP): Sintagma Inflexional</dt>
+                        <dd className="text-muted">Representa uma sentença</dd>
+                <dt > (DP): Sintagma Determinante</dt>
                         <dd className="text-muted">D: Artigos, Pronomes Demonstrativos, Pronomes Pessoais</dd>
-                    <dt>(SN) Sintagma Nominal </dt>
+                    <dt>(NP) Sintagma Nominal </dt>
                         <dd className="text-muted">N: Substantivos</dd>
-                    <dt> (SP): Sintagma Preposicional</dt>
+                    <dt> (PP): Sintagma Preposicional</dt>
                         <dd className="text-muted">P: Preposições</dd>
-                    <dt> (SNum): Sintagma Numeral</dt>
+                    <dt> (NumP): Sintagma Numeral</dt>
                         <dd className="text-muted">Num: Pronomes Numerais</dd>
-                    <dt> (SQ): Sintagma Quantificador</dt>
+                    <dt> (QP): Sintagma Quantificador</dt>
                         <dd className="text-muted">Q: Pronomes Indefinidos</dd>
-                    <dt> (SA): Sintagma Adjetival</dt>
+                    <dt> (AP): Sintagma Adjetival</dt>
                         <dd className="text-muted">A: Adjetivos</dd>
-                    <dt> (SAdv): Sintagma Adverbial</dt>
+                    <dt> (AdvP): Sintagma Adverbial</dt>
                         <dd className="text-muted">ADV: Adverbos</dd>
-                    <dt> (SV): Sintagma Verbal</dt>
+                    <dt> (VP): Sintagma Verbal</dt>
                         <dd className="text-muted">V: Verbos</dd>
-                    <dt> (SPoss): Sintagma Possesivo</dt>
+                    <dt> (PossP): Sintagma Possesivo</dt>
                         <dd className="text-muted">Poss: Pronomes Possessivos</dd>
                 </dl>
             </div>
